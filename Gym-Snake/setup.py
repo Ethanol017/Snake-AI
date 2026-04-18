70c6cf9c75b89933a95eb5529b81b14bf98752f4
Ethanol <ethanol017217@gmail.com>
2026-04-18 15:44:21 +0800

    chore: remove gym dependency from requirements and setup

--- Gym-Snake/setup.py
+++ Gym-Snake/setup.py
@@ -1,8 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='gym_snake',
       version='0.0.1',
       author="Satchel Grant",
-      install_requires=['gym', 'numpy', 'matplotlib'],
+      install_requires=['numpy', 'matplotlib'],
+      packages=find_packages(exclude=['logs']),
       python_requires='>=3',
-)
+      )
