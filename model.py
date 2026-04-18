6b3d4bb84dd455863b9e3c8d1ea2244eb290e9b1
Ethanol <ethanol017217@gmail.com>
2026-04-18 21:39:53 +0800

    refactor(model): remove unused layers and simplify forward pass

--- model.py
+++ model.py
@@ -7,31 +7,22 @@ class SnakePPO(nn.Module):
         super(SnakePPO, self).__init__()
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
-        
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-        
-        self.bn1 = nn.BatchNorm2d(16)
-        self.bn2 = nn.BatchNorm2d(32)
-        
 
-        self.fc1 = nn.Linear(32 * 3 * 3, 128)  
+        self.fc1 = nn.LazyLinear(128)  
         self.fc2 = nn.Linear(128, 64)
         
         self.policy_head = nn.Linear(64, 4)
         self.value_head = nn.Linear(64, 1)
         
-        self.dropout = nn.Dropout(0.2)
-
     def forward(self, x):
-        x = self.pool(F.relu(self.bn1(self.conv1(x))))
-        x = self.pool(F.relu(self.bn2(self.conv2(x))))
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
         # print(x.shape)
         x = x.reshape(x.size(0), -1)
         x = F.relu(self.fc1(x))
-        x = self.dropout(x)
         x = F.relu(self.fc2(x))
 
-        action_probs = F.softmax(self.policy_head(x), dim=-1)
+        action_logits = self.policy_head(x)
         state_value = self.value_head(x)
-        
-        return action_probs, state_value
+
+        return action_logits, state_value
