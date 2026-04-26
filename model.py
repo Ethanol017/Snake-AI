b67ce45268e3bb4ce54fd400362e0269893ec25a
Ethanol <ethanol017217@gmail.com>
2026-04-26 14:20:14 +0800

    feat(model): increase policy and value layers

--- model.py
+++ model.py
@@ -9,11 +9,14 @@ class SnakePPO(nn.Module):
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
         self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
 
-        self.fc1 = nn.LazyLinear(128)  
-        self.fc2 = nn.Linear(128, 64)
-        
-        self.policy_head = nn.Linear(64, 4)
-        self.value_head = nn.Linear(64, 1)
+        self.policy_fc1 = nn.LazyLinear(256)
+        self.policy_fc2 = nn.Linear(256, 128)
+        self.policy_head = nn.Linear(128, 4)
+
+        self.value_fc1 = nn.LazyLinear(256)
+        self.value_fc2 = nn.Linear(256, 256)
+        self.value_fc3 = nn.Linear(256, 128)
+        self.value_head = nn.Linear(128, 1)
         
     def forward(self, x):
         x = F.relu(self.conv1(x))
@@ -21,10 +24,13 @@ class SnakePPO(nn.Module):
         x = F.relu(self.conv3(x))
         # print(x.shape)
         x = x.reshape(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
+        policy_x = F.relu(self.policy_fc1(x))
+        policy_x = F.relu(self.policy_fc2(policy_x))
+        action_logits = self.policy_head(policy_x)
 
-        action_logits = self.policy_head(x)
-        state_value = self.value_head(x)
+        value_x = F.relu(self.value_fc1(x))
+        value_x = F.relu(self.value_fc2(value_x))
+        value_x = F.relu(self.value_fc3(value_x))
+        state_value = self.value_head(value_x)
 
         return action_logits, state_value
